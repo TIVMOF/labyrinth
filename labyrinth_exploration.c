d15fd2f0387d55314102d15914283b8d06e1d23c
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2023-06-09 23:32:48 +0300

    Update labyrinth_exploration.c

--- labyrinth_exploration.c
+++ labyrinth_exploration.c
@@ -1,93 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void labyrinth_to_file(char *file_name, int rows, int columns, int labyrinth[rows][columns]) {
-  FILE *file = fopen(file_name, "w");
-  if (file == NULL) {
-    printf("Failed to open the file.\n");
-    return;
-  }
+#include "visualization.h"
 
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      fprintf(file, "%d ", labyrinth[i][j]);
-      printf("%d ", labyrinth[i][j]);
-    }
-    fprintf(file, "\n");
-    printf("\n");
-  }
+void explore_labyrinth(int rows, int columns, int labyrinth[][columns], int* start_row, int* start_column, int direction) {
+    int new_row = *start_row;
+    int new_column = *start_column;
 
-  fclose(file);
-
-  printf("Labyrinth successfully written to file: %s\n", file_name);
-}
-
-int **file_to_labyrinth(char *file_name, int *rows, int *columns) {
-  FILE *file = fopen(file_name, "r");
-  if (file == NULL) {
-    printf("Failed to open the file.\n");
-    return NULL;
-  }
-
-  int curr_columns = 0;
-  int has_content = 0;
-  
-  char character;
-  
-  while ((character = fgetc(file)) != EOF) {
-    if (character == ' '){
-      continue;
-    }
-      
-    else if (character == '\n') {
-      if (has_content) {
-        (*rows)++;
-        if ((*columns) == 0)
-          (*columns) = curr_columns;
-          
-        else if (curr_columns != (*columns)) {
-          fclose(file);
-          printf("Incorrect file format!");
-          return NULL;
-        }
+    switch (direction){
+      case 1: {
+        new_column = new_column - 1;
+        break;
       }
-      
-      curr_columns = 0;
-      has_content = 0;
 
+      case 2: {
+        new_column = new_column + 1;
+        break;
+      }
+
+      case 3: {
+        new_row = new_row - 1;
+        break;
+      }
+
+      case 4: {
+        new_row = new_row + 1;
+        break;
+      }
+
+      case 5: {
+        return;
+      }
+    }
+
+    if (new_row >= 0 && new_row < rows && new_column >= 0 && new_column < columns) {
+        if (labyrinth[new_column][new_row] == 0) {
+            labyrinth[*start_column][*start_row] = 0;
+            labyrinth[new_column][new_row] = 2;
+
+            *start_row = new_row;
+            *start_column = new_column;
+
+            visualize_labyrinth(rows, columns, labyrinth, *start_row, *start_column);
+        } 
+        
+        else {
+            printf("Cannot move into a wall!\n");
+        }
     } 
-    
+      
     else {
-      curr_columns++;
-      has_content = 1;
+        printf("Cannot move outside the Labyrinth\n\n");
     }
-  }
-
-  // Reseting file pointer to the start of the file
-  fseek(file, 0, SEEK_SET);
-
-  int **labyrinth = (int **)malloc(*rows * sizeof(int *));
-
-  for (int i = 0; i < *rows; i++) {
-    labyrinth[i] = (int *)malloc(*columns * sizeof(int));
-
-    for (int j = 0; j < *columns; j++) {
-      fscanf(file, "%d", &labyrinth[i][j]);
-    }
-  }
-
-  fclose(file);
-
-  printf("Labyrinth successfully read from file: %s\n", file_name);
-
-  return labyrinth;
-}
-
-void dynamic_to_static_matrix(int rows, int columns, int **dynamic_matrix, int static_matrix[rows][columns]) {
-
-  for (int i = 0; i < rows; i++) {
-    for (int j = 0; j < columns; j++) {
-      static_matrix[i][j] = dynamic_matrix[i][j];
-    }
-  }
 }

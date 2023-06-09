f09d366448cb3bd6710ba80e15b36b17c702ce69
TIVMOF <78173711+TIVMOF@users.noreply.github.com>
2023-06-09 23:13:37 +0300

    Update labyrinth_exploration.c

--- labyrinth_exploration.c
+++ labyrinth_exploration.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 
-void explore_labyrinth(int rows, int columns, int labyrinth[][cols], int* start_row, int* start_column, int direction) {
+#include "visualization.h"
+
+void explore_labyrinth(int rows, int columns, int labyrinth[][columns], int* start_row, int* start_column, int direction) {
     int new_row = *start_row;
     int new_column = *start_column;
 
-    if (direction == 1 && *start_row > 0) {  // Up
-        new_row = *start_row - 1;
-    } 
-    
-    else if (direction == 2 && *start_row < rows - 1) {  // Down
-        new_row = *start_row + 1;
-    } 
-    
-    else if (direction == 3 && *start_column > 0) {  // Left
-        new_column = *start_column - 1;
-    } 
-    
-    else if (direction == 4 && *start_column < columns - 1) {  // Right
-        new_column = *start_column + 1;
+    switch (direction){
+      case 1: {
+        new_column = new_column - 1;
+        break;
+      }
+
+      case 2: {
+        new_column = new_column + 1;
+        break;
+      }
+
+      case 3: {
+        new_row = new_row - 1;
+        break;
+      }
+
+      case 4: {
+        new_row = new_row + 1;
+        break;
+      }
+
+      case 5: {
+        return;
+      }
     }
 
     if (new_row >= 0 && new_row < rows && new_column >= 0 && new_column < columns) {
-        if (labyrinth[new_row][new_column] == 0) {
-            labyrinth[*start_row][*start_column] = 0;
-            labyrinth[new_row][new_column] = 2;
+        if (labyrinth[new_column][new_row] == 0) {
+            labyrinth[*start_column][*start_row] = 0;
+            labyrinth[new_column][new_row] = 2;
 
             *start_row = new_row;
             *start_column = new_column;
+
+            visualize_labyrinth(rows, columns, labyrinth, *start_row, *start_column);
         } 
         
         else {
@@ -35,6 +49,6 @@ void explore_labyrinth(int rows, int columns, int labyrinth[][cols], int* start_
     } 
       
     else {
-        printf("Cannot move outside the matrix!\n");
+        printf("Cannot move outside the Labyrinth\n\n");
     }
 }
